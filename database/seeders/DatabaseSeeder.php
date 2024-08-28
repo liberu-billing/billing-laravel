d558eb2c7e4845d90fbcd8fbacd1e0865f133a90
raza9798 <m.raza9798@gmail.com>
2024-08-29 02:51:06 +0530

    login fix and site helper fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SiteSettingsSeeder::class,
             MenuSeeder::class,
             RolesSeeder::class,
             TeamSeeder::class,
